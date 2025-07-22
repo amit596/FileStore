41463f5f40898d60bd266f6180748ed2f11ea833
amit596 <kaliraman449@gmail.com>
2025-07-22 06:37:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,10 +40,10 @@ START_PIC = os.environ.get("START_PIC", "https://envs.sh/em9.jpg")
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/emH.jpg")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "linkshortify.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "c8c51280ae7b73b07b278a6ec131c3fb9d1598c5")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 3600)) # Add time in seconds
-TUT_VID = os.environ.get("TUT_VID","https://t.me/how_to_open_link_ak/18")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "gplinks.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "28a9d50fc1c6c070c68992e157fcd0fd9d3a9f2a")
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
+TUT_VID = os.environ.get("TUT_VID","https://t.me/how_to_open_link_ak/16")
 
 #--------------------------------------------
 
